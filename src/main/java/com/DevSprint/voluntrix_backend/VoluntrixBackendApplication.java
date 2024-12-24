6bcf5b3a958d0b4733d18f1ecfede10f6dc0f536
hadithya <hhadithya34@gmail.com>
2024-12-24 11:41:14 +0530

    Initial commit with Spring Boot setup

---
+++ src/main/java/com/DevSprint/voluntrix_backend/VoluntrixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.DevSprint.voluntrix_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntrixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntrixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevSprint/voluntrix_backend/VoluntrixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevSprint.voluntrix_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntrixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
