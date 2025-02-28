60baead3d99136fb43659a58f6723417c9f11cf0
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-02-28 21:01:34 +0530

    feat(repository): add OrganizationRepository for database operations

---
+++ src/main/java/com/DevSprint/voluntrix_backend/repositories/OrganizationRepository.java
@@ -0,0 +1,14 @@
+package com.DevSprint.voluntrix_backend.repositories;
+
+import com.DevSprint.voluntrix_backend.entities.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+
+    Optional<Organization> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
