8dd7f6f11a6e74abb892a2a0a7aa0f881c7e973d
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-02 12:13:05 +0530

    feat: add Event service interface and implementation

---
+++ src/main/java/com/DevSprint/voluntrix_backend/services/EventService.java
@@ -0,0 +1,5 @@
+package com.DevSprint.voluntrix_backend.services;
+
+public interface EventService {
+
+}
---
+++ src/main/java/com/DevSprint/voluntrix_backend/services/impl/EventServiceImpl.java
@@ -0,0 +1,10 @@
+package com.DevSprint.voluntrix_backend.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.DevSprint.voluntrix_backend.services.EventService;
+
+@Service
+public class EventServiceImpl implements EventService{
+
+}
