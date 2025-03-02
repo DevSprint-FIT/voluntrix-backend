a298a7b2e1ec1c6b63126a12796fc99978b95fe6
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-02 12:12:30 +0530

    feat: add Event repository to handle CRUD operations

---
+++ src/main/java/com/DevSprint/voluntrix_backend/repositories/EventRepository.java
@@ -0,0 +1,10 @@
+package com.DevSprint.voluntrix_backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.DevSprint.voluntrix_backend.entities.EventEntity;
+
+@Repository
+public interface EventRepository extends JpaRepository<EventEntity, Long>{
+}
