b314b8dd05028c026063ff4b51b2652383060702
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-02 21:29:19 +0530

    feat: create post mapping & add validation

--- src/main/java/com/DevSprint/voluntrix_backend/controllers/EventController.java
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/EventController.java
@@ -1,13 +1,35 @@
 package com.DevSprint.voluntrix_backend.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.DevSprint.voluntrix_backend.dtos.EventDTO;
+import com.DevSprint.voluntrix_backend.services.EventService;
+
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/public/v1/events")
 @SecurityRequirement(name = "bearerAuth")
+@RequiredArgsConstructor
 public class EventController {
 
+    private final EventService eventService;
+    
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> addEvent(@RequestBody EventDTO eventDTO) {
+        
+        if (eventDTO == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        
+        eventService.addEvent(eventDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
