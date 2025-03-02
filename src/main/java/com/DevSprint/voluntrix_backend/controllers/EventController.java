ec27451bc70d27fd0bd0c293a69523e7b3ea3ae9
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-02 14:01:27 +0530

    feat: add bearerAuth sequrity requirements

--- src/main/java/com/DevSprint/voluntrix_backend/controllers/EventController.java
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/EventController.java
@@ -3,8 +3,11 @@ package com.DevSprint.voluntrix_backend.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 @RequestMapping("/api/public/v1/events")
+@SecurityRequirement(name = "bearerAuth")
 public class EventController {
 
 }
