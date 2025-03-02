2f40f1a848803ab522e86dda95056764988a1322
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-02 12:10:15 +0530

    feat: create Event DTO, Entity, and Controller for managing events

---
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/EventController.java
@@ -0,0 +1,10 @@
+package com.DevSprint.voluntrix_backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public/v1/events")
+public class EventController {
+
+}
