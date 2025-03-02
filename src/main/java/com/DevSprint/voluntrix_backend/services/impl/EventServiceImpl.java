8543c6463ff2bd66405104a72f883867c130612f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-02 22:23:49 +0530

    feat: create deleteevent method

--- src/main/java/com/DevSprint/voluntrix_backend/services/impl/EventServiceImpl.java
+++ src/main/java/com/DevSprint/voluntrix_backend/services/impl/EventServiceImpl.java
@@ -3,6 +3,7 @@ package com.DevSprint.voluntrix_backend.services.impl;
 import org.springframework.stereotype.Service;
 
 import com.DevSprint.voluntrix_backend.dtos.EventDTO;
+import com.DevSprint.voluntrix_backend.exceptions.EventNotFoundException;
 import com.DevSprint.voluntrix_backend.repositories.EventRepository;
 import com.DevSprint.voluntrix_backend.services.EventService;
 import com.DevSprint.voluntrix_backend.utils.EntityDTOConvert;
@@ -22,5 +23,11 @@ public class EventServiceImpl implements EventService{
     public void addEvent(EventDTO eventDTO) {
         var eventEntity = entityDTOConvert.toEventEntity(eventDTO);
         eventRepository.save(eventEntity);
+    }
+
+    @Override
+    public void deleteEvent(Long eventId) {
+        eventRepository.findById(eventId).orElseThrow(() -> new EventNotFoundException("Event not found"));
+        eventRepository.deleteById(eventId);
     }
 }
