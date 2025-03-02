26d7978bc3eb229cfa7b777a0e7afaf82d1f99aa
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-02 22:23:06 +0530

    feat: create custom event not found exception

---
+++ src/main/java/com/DevSprint/voluntrix_backend/exceptions/EventNotFoundException.java
@@ -0,0 +1,8 @@
+package com.DevSprint.voluntrix_backend.exceptions;
+
+public class EventNotFoundException extends RuntimeException{
+
+    public EventNotFoundException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+}
