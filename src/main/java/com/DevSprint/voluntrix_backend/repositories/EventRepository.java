e5cf98c9cd55d349bb74e849a59e89cbca23309b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-03 15:14:40 +0530

    feaat: addd JpaSpecificationExecutor

--- src/main/java/com/DevSprint/voluntrix_backend/repositories/EventRepository.java
+++ src/main/java/com/DevSprint/voluntrix_backend/repositories/EventRepository.java
@@ -1,10 +1,11 @@
 package com.DevSprint.voluntrix_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.DevSprint.voluntrix_backend.entities.EventEntity;
 
 @Repository
-public interface EventRepository extends JpaRepository<EventEntity, Long> {
+public interface EventRepository extends JpaRepository<EventEntity, Long>, JpaSpecificationExecutor<EventEntity> {
 }
