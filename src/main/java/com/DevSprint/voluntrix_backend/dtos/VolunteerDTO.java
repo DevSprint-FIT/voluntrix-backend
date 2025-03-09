c2a32231a2b9f0abd93ee3405d3e8eb99bf71faf
Sanidi-Sisara <sanidi1113@gmail.com>
2025-03-09 21:12:05 +0530

    feat: add required attributes to VolunteerDTO

--- src/main/java/com/DevSprint/voluntrix_backend/dtos/VolunteerDTO.java
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/VolunteerDTO.java
@@ -2,11 +2,15 @@ package com.DevSprint.voluntrix_backend.dtos;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VolunteerDTO {
     private Long volunteerId;
     private String firstName;
@@ -16,4 +20,6 @@ public class VolunteerDTO {
     private String availabilityStatus;
     private List<String> interestAreas;
     private List<String> preferredOrganizations;
+    private Integer volunteerLevel; 
+    private Integer rewardPoints;   
 }
