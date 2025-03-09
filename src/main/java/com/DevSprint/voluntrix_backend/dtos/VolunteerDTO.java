8b1ad9e129da2ea22765e7e98f86a30b1a1ecf57
Sanidi-Sisara <sanidi1113@gmail.com>
2025-03-09 11:51:08 +0530

    feat: add VolunteerDTO for data transfer

---
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/VolunteerDTO.java
@@ -0,0 +1,19 @@
+package com.DevSprint.voluntrix_backend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class VolunteerDTO {
+    private Long volunteer_id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String institute;
+    private String availabilityStatus;
+    private List<String> interestAreas;
+    private List<String> preferredOrganizations;
+}
