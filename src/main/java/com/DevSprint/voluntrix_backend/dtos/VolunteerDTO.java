cae86952b59158433429faf58898a7f4b98a125a
Sanidi-Sisara <sanidi1113@gmail.com>
2025-03-09 21:10:20 +0530

    feat: add all required attributes to VolunteerDTO

--- src/main/java/com/DevSprint/voluntrix_backend/dtos/VolunteerDTO.java
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/VolunteerDTO.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Getter
 @Setter
 public class VolunteerDTO {
-    private Long volunteer_id;
+    private Long volunteerId;
     private String firstName;
     private String lastName;
     private String email;
