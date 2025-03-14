6a3acc8b3eb4f16c66f1b5ff5737b0c3c23d155c
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-03-14 10:26:53 +0530

    chore: update application configuration

--- src/main/java/com/DevSprint/voluntrix_backend/VoluntrixBackendApplication.java
+++ src/main/java/com/DevSprint/voluntrix_backend/VoluntrixBackendApplication.java
@@ -2,12 +2,12 @@ package com.DevSprint.voluntrix_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.DevSprint.voluntrix_backend.repositories")
 public class VoluntrixBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VoluntrixBackendApplication.class, args);
 	}
-
 }
