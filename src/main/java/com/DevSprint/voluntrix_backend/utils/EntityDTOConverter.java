ec9d4723d135b1722a852609b2c0710c27a9abd5
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-03-14 19:04:37 +0530

    feat(utils): add EntityDTOConverter utility class for mapping between entity and DTO

---
+++ src/main/java/com/DevSprint/voluntrix_backend/utils/EntityDTOConverter.java
@@ -0,0 +1,50 @@
+package com.DevSprint.voluntrix_backend.utils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.DevSprint.voluntrix_backend.dtos.OrganizationDTO;
+import com.DevSprint.voluntrix_backend.entities.Organization;
+
+@Component
+public class EntityDTOConverter {
+
+    // Organization to OrganizationDTO
+    public OrganizationDTO toOrganizationDTO(Organization organization) {
+        var organizationDTO = new OrganizationDTO();
+
+        organizationDTO.setId(organization.getId());
+        organizationDTO.setName(organization.getName());
+        organizationDTO.setEmail(organization.getEmail());
+        organizationDTO.setPhone(organization.getPhone());
+        organizationDTO.setAccountNumber(organization.getAccountNumber());
+        organizationDTO.setIsVerified(organization.getIsVerified());
+        organizationDTO.setFollowerCount(organization.getFollowerCount());
+
+        return organizationDTO;
+    }
+
+    // OrganizationDTO to Organization
+    public Organization toOrganizationEntity(OrganizationDTO organizationDTO) {
+        var organization = new Organization();
+
+        organization.setId(organizationDTO.getId());
+        organization.setName(organizationDTO.getName());
+        organization.setEmail(organizationDTO.getEmail());
+        organization.setPhone(organizationDTO.getPhone());
+        organization.setAccountNumber(organizationDTO.getAccountNumber());
+        organization.setIsVerified(organizationDTO.getIsVerified());
+        organization.setFollowerCount(organizationDTO.getFollowerCount());
+
+        return organization;
+    }
+
+    // List<Organization> to List<OrganizationDTO>
+    public List<OrganizationDTO> toOrganizationDTOList(List<Organization> organizationList) {
+        return organizationList.stream()
+                .map(this::toOrganizationDTO)
+                .collect(Collectors.toList());
+    }
+}
