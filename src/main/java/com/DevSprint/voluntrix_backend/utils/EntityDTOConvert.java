9bbe206ffafe7710dfb328d64fb3f65713fc0d67
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-22 23:00:41 +0530

    feat: automate mapping wiht modelmapper

--- src/main/java/com/DevSprint/voluntrix_backend/utils/EntityDTOConvert.java
+++ src/main/java/com/DevSprint/voluntrix_backend/utils/EntityDTOConvert.java
@@ -3,79 +3,34 @@ package com.DevSprint.voluntrix_backend.utils;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.DevSprint.voluntrix_backend.dtos.EventDTO;
 import com.DevSprint.voluntrix_backend.entities.EventEntity;
 
-// Manual mapping
-// Can be automated with ModelMapper
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class EntityDTOConvert {
 
-    //Event Mapping
+    // Event Mapping
+    private final ModelMapper modelMapper;
 
     // EventEntity to EventDTO
     public EventDTO toEventDTO(EventEntity eventEntity) {
-
-        var eventDTO = new EventDTO();
-
-        eventDTO.setEventId(eventEntity.getEventId());
-        eventDTO.setEventTitle(eventEntity.getEventTitle());
-        eventDTO.setEventDescription(eventEntity.getEventDescription());
-        eventDTO.setEventLocation(eventEntity.getEventLocation());
-        eventDTO.setEventDate(eventEntity.getEventDate());
-        eventDTO.setEventTime(eventEntity.getEventTime());
-        eventDTO.setEventImageUrl(eventEntity.getEventImageUrl());
-        eventDTO.setEventType(eventEntity.getEventType());
-        eventDTO.setEventVisibility(eventEntity.getEventVisibility());
-        eventDTO.setEventStatus(eventEntity.getEventStatus());
-        eventDTO.setSponsorshipEnabled(eventEntity.getSponsorshipEnabled());
-        eventDTO.setDonationEnabled(eventEntity.getDonationEnabled());
-
-        return eventDTO;
+        return modelMapper.map(eventEntity, EventDTO.class);
     }
 
     // EventDTO to EventEntity
     public EventEntity toEventEntity(EventDTO eventDTO) {
-
-        var eventEntity = new EventEntity();
-
-        eventEntity.setEventId(eventDTO.getEventId());
-        eventEntity.setEventTitle(eventDTO.getEventTitle());
-        eventEntity.setEventDescription(eventDTO.getEventDescription());
-        eventEntity.setEventLocation(eventDTO.getEventLocation());
-        eventEntity.setEventDate(eventDTO.getEventDate());
-        eventEntity.setEventTime(eventDTO.getEventTime());
-        eventEntity.setEventImageUrl(eventDTO.getEventImageUrl());
-        eventEntity.setEventType(eventDTO.getEventType());
-        eventEntity.setEventVisibility(eventDTO.getEventVisibility());
-        eventEntity.setEventStatus(eventDTO.getEventStatus());
-        eventEntity.setSponsorshipEnabled(eventDTO.getSponsorshipEnabled());
-        eventEntity.setDonationEnabled(eventDTO.getDonationEnabled());
-
-        return eventEntity;
+        return modelMapper.map(eventDTO, EventEntity.class);
     }
 
     // List<EventEntity> to List<EventDTO>
     public List<EventDTO> toEventDTOList(List<EventEntity> eventEntityList) {
-        return eventEntityList.stream().map(entity -> {
-            EventDTO eventDTO = new EventDTO();
-
-            eventDTO.setEventId(entity.getEventId());
-            eventDTO.setEventTitle(entity.getEventTitle());
-            eventDTO.setEventDescription(entity.getEventDescription());
-            eventDTO.setEventLocation(entity.getEventLocation());
-            eventDTO.setEventDate(entity.getEventDate());
-            eventDTO.setEventTime(entity.getEventTime());
-            eventDTO.setEventImageUrl(entity.getEventImageUrl());
-            eventDTO.setEventType(entity.getEventType());
-            eventDTO.setEventVisibility(entity.getEventVisibility());
-            eventDTO.setEventStatus(entity.getEventStatus());
-            eventDTO.setSponsorshipEnabled(entity.getSponsorshipEnabled());
-            eventDTO.setDonationEnabled(entity.getDonationEnabled());
-
-            return eventDTO;
-        }).collect(Collectors.toList());
+        return eventEntityList.stream().map(entity -> modelMapper.map(entity, EventDTO.class))
+                .collect(Collectors.toList());
     }
 }
