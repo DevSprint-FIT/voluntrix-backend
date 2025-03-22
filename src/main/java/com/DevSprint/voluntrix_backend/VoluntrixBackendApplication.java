0f74ca94df4532662ceb5dd3fc89496ac66fc089
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-22 23:01:01 +0530

    feat: add modelmmapper

--- src/main/java/com/DevSprint/voluntrix_backend/VoluntrixBackendApplication.java
+++ src/main/java/com/DevSprint/voluntrix_backend/VoluntrixBackendApplication.java
@@ -1,7 +1,9 @@
 package com.DevSprint.voluntrix_backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,6 +14,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableTransactionManagement
 public class VoluntrixBackendApplication {
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VoluntrixBackendApplication.class, args);
 	}
