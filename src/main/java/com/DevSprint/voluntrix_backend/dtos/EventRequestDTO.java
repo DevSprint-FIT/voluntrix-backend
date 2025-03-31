cba95021b8f22dd80807ab7b969ab4f5e1c3f555
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-03-31 15:55:56 +0530

    feat(dto): add EventRequestDTO to represent event request data

---
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/EventRequestDTO.java
@@ -0,0 +1,13 @@
+package com.DevSprint.voluntrix_backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventRequestDTO {
+    private String eventTitle;
+
+}
