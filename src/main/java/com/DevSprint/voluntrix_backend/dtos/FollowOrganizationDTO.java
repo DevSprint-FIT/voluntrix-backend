d5158287129fe0d96c8cbc5a8267be471d4f7cff
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-04-02 21:57:38 +0530

    refactor(dto): rename FollowOrganizationDTO

--- src/main/java/com/DevSprint/voluntrix_backend/dtos/FollowOrganizationDTO.java
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/FollowOrganizationDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class FollowOrganizationRequest {
+public class FollowOrganizationDTO {
     private Long id;
     private Long volunteerId;
     private Long organizationId;
