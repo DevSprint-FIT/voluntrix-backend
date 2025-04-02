5b5bd3d2c9d014848b3f6cca327c9642ae2dcf99
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-02 22:19:54 +0530

    refactor: use EntityDTOConvert for entity-DTO conversions in VolunteerService

--- src/main/java/com/DevSprint/voluntrix_backend/services/VolunteerService.java
+++ src/main/java/com/DevSprint/voluntrix_backend/services/VolunteerService.java
@@ -3,62 +3,37 @@ package com.DevSprint.voluntrix_backend.services;
 import com.DevSprint.voluntrix_backend.dtos.VolunteerDTO;
 import com.DevSprint.voluntrix_backend.entities.Volunteer;
 import com.DevSprint.voluntrix_backend.repositories.VolunteerRepository;
+import com.DevSprint.voluntrix_backend.utils.EntityDTOConvert;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class VolunteerService {
 
     private final VolunteerRepository volunteerRepository;
+    private final EntityDTOConvert entityDTOConvert;
 
-    public VolunteerService(VolunteerRepository volunteerRepository) {
-       this.volunteerRepository = volunteerRepository;
-    }
-
-    private VolunteerDTO convertToDTO(Volunteer volunteer) {
-        VolunteerDTO volunteerDTO = new VolunteerDTO();
-        volunteerDTO.setVolunteerId(volunteer.getVolunteerId());
-        volunteerDTO.setFirstName(volunteer.getFirstName());
-        volunteerDTO.setLastName(volunteer.getLastName());
-        volunteerDTO.setEmail(volunteer.getEmail());
-        volunteerDTO.setInstitute(volunteer.getInstitute());
-        volunteerDTO.setInterestAreas(volunteer.getInterestAreas());
-        volunteerDTO.setAvailabilityStatus(volunteer.getAvailabilityStatus());
-        volunteerDTO.setPreferredOrganizations(volunteer.getPreferredOrganizations());
-        volunteerDTO.setVolunteerLevel(volunteer.getVolunteerLevel());
-        volunteerDTO.setRewardPoints(volunteer.getRewardPoints());
-        return volunteerDTO;
+    public VolunteerService(VolunteerRepository volunteerRepository, EntityDTOConvert entityDTOConvert) {
+        this.volunteerRepository = volunteerRepository;
+        this.entityDTOConvert = entityDTOConvert;
     }
 
     public VolunteerDTO createVolunteer(VolunteerDTO volunteerDTO) {
-        Volunteer volunteer = new Volunteer();
-        volunteer.setFirstName(volunteerDTO.getFirstName());
-        volunteer.setLastName(volunteerDTO.getLastName());
-        volunteer.setEmail(volunteerDTO.getEmail());
-        volunteer.setInstitute(volunteerDTO.getInstitute());
-        volunteer.setInterestAreas(volunteerDTO.getInterestAreas());
-        volunteer.setAvailabilityStatus(volunteerDTO.getAvailabilityStatus());
-        volunteer.setPreferredOrganizations(volunteerDTO.getPreferredOrganizations());
-        volunteer.setVolunteerLevel(1);
-        volunteer.setRewardPoints(0);
-
+        Volunteer volunteer = entityDTOConvert.toVolunteerEntity(volunteerDTO);
         Volunteer savedVolunteer = volunteerRepository.save(volunteer);
-        return convertToDTO(savedVolunteer);
+        return entityDTOConvert.toVolunteerDTO(savedVolunteer);
     }
 
     public List<VolunteerDTO> getAllVolunteers() {
         List<Volunteer> volunteers = volunteerRepository.findAll();
-        return volunteers.stream()
-                         .map(this::convertToDTO)
-                         .collect(Collectors.toList());
+        return entityDTOConvert.toVolunteerDTOList(volunteers);
     }
 
     public VolunteerDTO getVolunteerById(Long volunteerId) {
         Optional<Volunteer> volunteer = volunteerRepository.findById(volunteerId);
-        return volunteer.map(this::convertToDTO).orElse(null);
+        return volunteer.map(entityDTOConvert::toVolunteerDTO).orElse(null);
     }
 
     public VolunteerDTO updateVolunteer(Long volunteerId, VolunteerDTO volunteerDTO) {
@@ -75,7 +50,7 @@ public class VolunteerService {
             volunteer.setPreferredOrganizations(volunteerDTO.getPreferredOrganizations());
 
             Volunteer updatedVolunteer = volunteerRepository.save(volunteer);
-            return convertToDTO(updatedVolunteer);
+            return entityDTOConvert.toVolunteerDTO(updatedVolunteer);
         } else {
             throw new RuntimeException("Volunteer not found with ID: " + volunteerId);
         }
