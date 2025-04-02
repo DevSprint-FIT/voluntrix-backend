1d7c7c23680910bbe2e900a9c7fc0249bb9f0ad6
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-02 22:40:48 +0530

    refactor: replace wildcard imports in Volunteer entity

--- src/main/java/com/DevSprint/voluntrix_backend/entities/Volunteer.java
+++ src/main/java/com/DevSprint/voluntrix_backend/entities/Volunteer.java
@@ -1,7 +1,16 @@
 package com.DevSprint.voluntrix_backend.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
