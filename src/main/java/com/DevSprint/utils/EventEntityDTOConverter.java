cec95dd56843a81326abadbb04aeff9e5091eb5d
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-04-03 11:10:10 +0530

    feat(utils): add utility functions for DTO conversion

---
+++ src/main/java/com/DevSprint/utils/EventEntityDTOConverter.java
@@ -0,0 +1,24 @@
+package com.DevSprint.voluntrix_backend.utils;
+
+import org.springframework.stereotype.Component;
+import com.DevSprint.voluntrix_backend.dtos.EventRequestDTO;
+import com.DevSprint.voluntrix_backend.entities.EventEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class EventEntityDTOConverter {
+
+    // Convert EventEntity to EventRequestDTO
+    public EventRequestDTO toEventRequestDTO(EventEntity event) {
+        return new EventRequestDTO(event.getEventTitle());
+    }
+
+    // Convert a List of EventEntity to List of EventRequestDTO
+    public List<EventRequestDTO> toEventRequestDTOList(List<EventEntity> events) {
+        return events.stream()
+                     .map(this::toEventRequestDTO)
+                     .collect(Collectors.toList());
+    }
+}
