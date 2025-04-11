c16453a212968789dace5dfecf9f323456035f02
hadithya <hhadithya34@gmail.com>
2025-04-11 09:18:16 +0530

    chore(wip): temp commit before switching branches

--- src/main/java/com/DevSprint/voluntrix_backend/config/SecurityConfig.java
+++ src/main/java/com/DevSprint/voluntrix_backend/config/SecurityConfig.java
@@ -2,7 +2,14 @@ package com.DevSprint.voluntrix_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,6 +18,8 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalAuthentication    // for role based authorization
 public class SecurityConfig {
 
     @Bean
@@ -20,18 +29,28 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                            "/swagger-ui/**", 
+                            "/swagger-ui/**",
                             "/v3/api-docs/**",
                             "/swagger-ui.html"
                         ).permitAll()
-                        .requestMatchers("/api/public/**").permitAll() // Allow all requests under `/api/public/`
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN") // Restrict `/api/admin/` to ADMIN role
+//                        .requestMatchers("/api/public/**").permitAll() // Allow all requests under `/api/public/`
+//                        .requestMatchers("/api/admin/**").hasRole("ADMIN") // Restrict `/api/admin/` to ADMIN role
+                        .requestMatchers("/", "/login", "/oauth2/**").permitAll()
                         .anyRequest().authenticated() // Require authentication for everything else
                 )
                 .formLogin(form -> form.disable())
-                .httpBasic(httpBasic -> httpBasic.disable());
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .oauth2Login(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
