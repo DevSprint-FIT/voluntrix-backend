a12937b7f1613903a6842ddca6ba29a2dd9f8106
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-04-17 22:39:35 +0530

    refactor(controller): improve HTTP response handling with ResponseEntity.ok

--- src/main/java/com/DevSprint/voluntrix_backend/controllers/FollowedOrganizationController.java
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/FollowedOrganizationController.java
@@ -1,9 +1,10 @@
 package com.DevSprint.voluntrix_backend.controllers;
 
-import com.DevSprint.voluntrix_backend.services.FollowedOrganizationService;
 import com.DevSprint.voluntrix_backend.dtos.FollowOrganizationDTO;
+import com.DevSprint.voluntrix_backend.services.FollowedOrganizationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,24 +17,28 @@ public class FollowedOrganizationController {
     @Autowired
     private FollowedOrganizationService followedOrganizationService;
 
-    
+
     @PostMapping("/")
-    public String followOrganization(@RequestBody FollowOrganizationDTO request) {
-        return followedOrganizationService.followOrganization(request.getVolunteerId(), request.getOrganizationId());
+    public ResponseEntity<String> followOrganization(@RequestBody FollowOrganizationDTO request) {
+        String response = followedOrganizationService.followOrganization(request.getVolunteerId(), request.getOrganizationId());
+        return ResponseEntity.ok(response);
     }
 
-    
+    // Unfollow an organization
     @DeleteMapping("/{volunteerId}/{organizationId}")
-    public String unfollowOrganization(@PathVariable Long volunteerId, @PathVariable Long organizationId) {
-        return followedOrganizationService.unfollowOrganization(volunteerId, organizationId);
+    public ResponseEntity<String> unfollowOrganization(@PathVariable Long volunteerId, @PathVariable Long organizationId) {
+        String response = followedOrganizationService.unfollowOrganization(volunteerId, organizationId);
+        return ResponseEntity.ok(response);
     }
 
-    
+
     @GetMapping("/{volunteerId}")
-    public List<Long> getFollowedOrganizations(@PathVariable Long volunteerId) {
-       return followedOrganizationService.getFollowedOrganizations(volunteerId)
-                                      .stream()
-                                      .map(FollowOrganizationDTO::getOrganizationId) 
-                                      .collect(Collectors.toList());
+    public ResponseEntity<List<Long>> getFollowedOrganizations(@PathVariable Long volunteerId) {
+        List<Long> organizationIds = followedOrganizationService.getFollowedOrganizations(volunteerId)
+                .stream()
+                .map(FollowOrganizationDTO::getOrganizationId)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(organizationIds);
     }
 }
