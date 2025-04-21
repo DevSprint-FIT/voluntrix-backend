9b9df7b4d8672617eefac889c8acae6bc3d5c9da
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-21 19:32:05 +0530

    refactor: replace PUT with PATCH for partial updates in VolunteerController

--- src/main/java/com/DevSprint/voluntrix_backend/controllers/VolunteerController.java
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/VolunteerController.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,9 +48,9 @@ public class VolunteerController {
         return ResponseEntity.status(201).body(createdVolunteer);
     }
 
-    @PutMapping("/{volunteerId}")
+    @PatchMapping("/{volunteerId}")
     public ResponseEntity<VolunteerDTO> updateVolunteer(@PathVariable Long volunteerId, @RequestBody VolunteerDTO volunteerDTO) {
-        VolunteerDTO updatedVolunteer = volunteerService.updateVolunteer(volunteerId, volunteerDTO);
+        VolunteerDTO updatedVolunteer = volunteerService.patchVolunteer(volunteerId, volunteerDTO);
         if (updatedVolunteer != null) {
             return ResponseEntity.ok(updatedVolunteer);
         } else {
