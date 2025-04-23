f011ea4b08e3a0e82a0d4ecd7b95f41f1ba9f8aa
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-24 00:14:33 +0530

    refactor: update VolunteerController to use VolunteerCreateDTO and VolunteerUpdateDTO

--- src/main/java/com/DevSprint/voluntrix_backend/controllers/VolunteerController.java
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/VolunteerController.java
@@ -1,17 +1,12 @@
 package com.DevSprint.voluntrix_backend.controllers;
 
 import com.DevSprint.voluntrix_backend.dtos.VolunteerDTO;
+import com.DevSprint.voluntrix_backend.dtos.VolunteerCreateDTO;
+import com.DevSprint.voluntrix_backend.dtos.VolunteerUpdateDTO;
 import com.DevSprint.voluntrix_backend.services.VolunteerService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,14 +38,14 @@ public class VolunteerController {
     }
 
     @PostMapping
-    public ResponseEntity<VolunteerDTO> createVolunteer(@RequestBody VolunteerDTO volunteerDTO) {
-        VolunteerDTO createdVolunteer = volunteerService.createVolunteer(volunteerDTO);
+    public ResponseEntity<VolunteerDTO> createVolunteer(@RequestBody VolunteerCreateDTO volunteerCreateDTO) {
+        VolunteerDTO createdVolunteer = volunteerService.createVolunteer(volunteerCreateDTO);
         return ResponseEntity.status(201).body(createdVolunteer);
     }
 
     @PatchMapping("/{volunteerId}")
-    public ResponseEntity<VolunteerDTO> updateVolunteer(@PathVariable Long volunteerId, @RequestBody VolunteerDTO volunteerDTO) {
-        VolunteerDTO updatedVolunteer = volunteerService.patchVolunteer(volunteerId, volunteerDTO);
+    public ResponseEntity<VolunteerDTO> updateVolunteer(@PathVariable Long volunteerId, @RequestBody VolunteerUpdateDTO volunteerUpdateDTO) {
+        VolunteerDTO updatedVolunteer = volunteerService.patchVolunteer(volunteerId, volunteerUpdateDTO);
         if (updatedVolunteer != null) {
             return ResponseEntity.ok(updatedVolunteer);
         } else {
@@ -62,10 +57,9 @@ public class VolunteerController {
     public ResponseEntity<Void> deleteVolunteer(@PathVariable Long volunteerId) {
         boolean isDeleted = volunteerService.deleteVolunteer(volunteerId);
         if (isDeleted) {
-            return ResponseEntity.noContent().build();  
+            return ResponseEntity.noContent().build();
         } else {
-            return ResponseEntity.notFound().build();  
+            return ResponseEntity.notFound().build();
         }
     }
 }
-
