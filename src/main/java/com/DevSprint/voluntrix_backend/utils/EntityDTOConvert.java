727f249c3896c9d4ace4dfb55feb49acdb9cf72a
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-23 23:20:09 +0530

    refactor: extend EntityDTOConvert to support VolunteerCreateDTO and VolunteerUpdateDTO mappings

--- src/main/java/com/DevSprint/voluntrix_backend/utils/EntityDTOConvert.java
+++ src/main/java/com/DevSprint/voluntrix_backend/utils/EntityDTOConvert.java
@@ -7,6 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.DevSprint.voluntrix_backend.dtos.VolunteerDTO;
+import com.DevSprint.voluntrix_backend.dtos.VolunteerCreateDTO;
+import com.DevSprint.voluntrix_backend.dtos.VolunteerUpdateDTO;
 import com.DevSprint.voluntrix_backend.entities.Volunteer;
 
 import lombok.RequiredArgsConstructor;
@@ -17,17 +19,22 @@ public class EntityDTOConvert {
 
     private final ModelMapper modelMapper;
 
-    // VolunteerEntity to VolunteerDTO
     public VolunteerDTO toVolunteerDTO(Volunteer volunteer) {
         return modelMapper.map(volunteer, VolunteerDTO.class);
     }
 
-    // VolunteerDTO to VolunteerEntity
     public Volunteer toVolunteerEntity(VolunteerDTO volunteerDTO) {
         return modelMapper.map(volunteerDTO, Volunteer.class);
     }
 
-    // List<Volunteer> to List<VolunteerDTO>
+    public Volunteer toVolunteerEntity(VolunteerCreateDTO volunteerCreateDTO) {
+        return modelMapper.map(volunteerCreateDTO, Volunteer.class);
+    }
+
+    public Volunteer toVolunteerEntity(VolunteerUpdateDTO volunteerUpdateDTO) {
+        return modelMapper.map(volunteerUpdateDTO, Volunteer.class);
+    }
+
     public List<VolunteerDTO> toVolunteerDTOList(List<Volunteer> volunteers) {
         return volunteers.stream()
                 .map(entity -> modelMapper.map(entity, VolunteerDTO.class))
