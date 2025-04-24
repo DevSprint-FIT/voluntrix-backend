3a9b19b91b53808b2bc4c547b28ba4acce36ca19
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-04-24 21:27:55 +0530

    feat(entity): add Volunteer entity

---
+++ src/main/java/com/DevSprint/voluntrix_backend/entities/Volunteer.java
@@ -0,0 +1,55 @@
+package com.DevSprint.voluntrix_backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "volunteer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Volunteer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long volunteerId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String institute;
+
+    @ElementCollection
+    private List<String> interestAreas;
+
+    @Column(nullable = false)
+    private String availabilityStatus;
+
+    @ElementCollection
+    private List<String> preferredOrganizations;
+
+    @Column(nullable = false)
+    private Integer volunteerLevel = 1;
+
+    @Column(nullable = false)
+    private Integer rewardPoints = 0;
+}
