c611ae37f556c4602eef1e40a4f37a57b08482af
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-04-25 19:25:07 +0530

    feat: add EventDTO class for event data transfer

---
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/EventDTO.java
@@ -0,0 +1,18 @@
+package com.DevSprint.voluntrix_backend.dtos;
+
+import com.DevSprint.voluntrix_backend.enums.EventStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventDTO {
+    private String eventTitle;
+    private LocalDate eventDate;
+    private String eventLocation;
+    private EventStatus eventStatus;
+}
