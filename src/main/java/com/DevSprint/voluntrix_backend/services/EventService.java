4f14aa7921d8f1bb2018a752a9820dbfe923a65f
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-04-25 19:26:03 +0530

    feat: add EventService for fetching active events by organization

---
+++ src/main/java/com/DevSprint/voluntrix_backend/services/EventService.java
@@ -0,0 +1,33 @@
+package com.DevSprint.voluntrix_backend.services;
+
+import com.DevSprint.voluntrix_backend.dtos.EventDTO;
+import com.DevSprint.voluntrix_backend.entities.EventEntity;
+import com.DevSprint.voluntrix_backend.enums.EventStatus;
+import com.DevSprint.voluntrix_backend.repositories.EventRepository;
+import com.DevSprint.voluntrix_backend.utils.EventEntityDTOConverter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private EventEntityDTOConverter converter;
+
+    public List<EventDTO> getActiveEventsByOrganization(Long organizationId) {
+        List<EventEntity> activeEvents = eventRepository.findByOrganizationIdAndEventStatus(
+                organizationId,
+                EventStatus.ACTIVE
+        );
+        return converter.toEventDTOList(activeEvents);
+    }
+
+
+}
