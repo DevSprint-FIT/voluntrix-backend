8e2031a12b5e5ce32cb07f1e03d7f4af90ebec40
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-26 19:30:43 +0530

    feat: add validations to VolunteerUpdateDTO

--- src/main/java/com/DevSprint/voluntrix_backend/dtos/VolunteerUpdateDTO.java
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/VolunteerUpdateDTO.java
@@ -1,5 +1,7 @@
 package com.DevSprint.voluntrix_backend.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,18 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class VolunteerUpdateDTO {
     private String firstName;
+
     private String lastName;
+
+    @Email(message = "Email should be valid")
     private String email;
+
     private String institute;
+
     private Boolean isAvailable;
+
     private Boolean isEventHost;
+
+    @Size(max = 255, message = "About section can have at most 255 characters")
     private String about;
 }
