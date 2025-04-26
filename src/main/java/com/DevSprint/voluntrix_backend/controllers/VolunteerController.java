9e26eb15392b2dadb60a146b9e76a4c7f78b4da0
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-26 10:30:11 +0530

    chore: clean up imports by removing unnecessary annotation packages

--- src/main/java/com/DevSprint/voluntrix_backend/controllers/VolunteerController.java
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/VolunteerController.java
@@ -6,7 +6,14 @@ import com.DevSprint.voluntrix_backend.dtos.VolunteerUpdateDTO;
 import com.DevSprint.voluntrix_backend.services.VolunteerService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
