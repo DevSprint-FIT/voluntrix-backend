759c9d8f175bbcec6d393683b5e6362956037d28
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-28 23:07:18 +0530

    feat: add max length validation to description in TaskCreateDTO

--- src/main/java/com/DevSprint/voluntrix_backend/dtos/TaskCreateDTO.java
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/TaskCreateDTO.java
@@ -4,6 +4,7 @@ import com.DevSprint.voluntrix_backend.enums.TaskStatus;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Future;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +19,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class TaskCreateDTO {
     @NotBlank(message = "Task description must not be blank")
+    @Size(max = 500, message = "Description can be up to 500 characters")
     private String description;
 
     @NotNull(message = "Due date is required")
