b057a0912658a6edf4b54dee23fab84335947d8a
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-28 23:16:39 +0530

    feat: add optional-safe field validations to TaskUpdateDTO

--- src/main/java/com/DevSprint/voluntrix_backend/dtos/TaskUpdateDTO.java
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/TaskUpdateDTO.java
@@ -1,6 +1,9 @@
 package com.DevSprint.voluntrix_backend.dtos;
 
 import com.DevSprint.voluntrix_backend.enums.TaskStatus;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -13,10 +16,18 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskUpdateDTO {
-    private String description;           
-    private LocalDateTime dueDate;        
+    @Size(max = 500, message = "Description can be up to 500 characters")
+    private String description;
+
+    @Future(message = "Due date must be in the future")
+    private LocalDateTime dueDate;
+
     private TaskStatus taskStatus; // TO_DO, IN_PROGRESS, DONE
-    private String resourceUrl;           
-    private LocalDateTime taskSubmittedDate; 
+
+    @Size(max = 1000, message = "Resource URL is too long")
+    private String resourceUrl;
+
+    private LocalDateTime taskSubmittedDate;
+
     private Long assigneeId;
 }
