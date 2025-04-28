e78698c6ca451dfef26a86126156bcb7d3428622
Sanidi-Sisara <sanidi1113@gmail.com>
2025-04-28 19:18:37 +0530

    feat: add TaskRepository interface for task data access

---
+++ src/main/java/com/DevSprint/voluntrix_backend/repositories/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.DevSprint.voluntrix_backend.repositories;
+
+import com.DevSprint.voluntrix_backend.entities.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+    List<TaskEntity> findByAssignee_VolunteerId(Long assigneeId);
+    List<TaskEntity> findByEvent_EventId(Long eventId);
+}
