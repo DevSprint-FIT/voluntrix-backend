256671cb81258cd72df54c5726e22d273c654bf1
Nuraj Perera <nurajsp@gmail.com>
2025-05-18 19:54:03 +0530

    feat : configured the MessageWebSocketController

---
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/MessageWebSocketController.java
@@ -0,0 +1,25 @@
+package com.DevSprint.voluntrix_backend.controllers;
+
+import com.DevSprint.voluntrix_backend.dtos.MessageDTO;
+import com.DevSprint.voluntrix_backend.entities.Message;
+import com.DevSprint.voluntrix_backend.services.MessageService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MessageWebSocketController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final MessageService messageService;
+
+    @MessageMapping("/chat/send")
+    public void sendMessage(MessageDTO dto) {
+        Message saved = messageService.saveMessage(dto);
+        messagingTemplate.convertAndSend("/topic/chat/" + dto.getReceiverId(), saved);
+    }
+}
