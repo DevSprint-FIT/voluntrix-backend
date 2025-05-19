17ae1d2936a58a2f80e39d40a0ce76f142d4bdcb
Nuraj Perera <nurajsp@gmail.com>
2025-05-19 17:24:05 +0530

    update : removed websockets temporary

--- src/main/java/com/DevSprint/voluntrix_backend/config/WebSocketConfig.java
+++ src/main/java/com/DevSprint/voluntrix_backend/config/WebSocketConfig.java
@@ -1,25 +1,25 @@
-package com.DevSprint.voluntrix_backend.config;
+// package com.DevSprint.voluntrix_backend.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+// @Configuration
+// @EnableWebSocketMessageBroker
+// public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
-@Override
-public void registerStompEndpoints(StompEndpointRegistry registry){
+// @Override
+// public void registerStompEndpoints(StompEndpointRegistry registry){
 
-    registry.addEndpoint("/ws-chat").setAllowedOrigins("*").withSockJS();
-}
+//     registry.addEndpoint("/ws-chat").setAllowedOrigins("*").withSockJS();
+// }
 
-@Override
-public void configureMessageBroker(MessageBrokerRegistry registry){
+// @Override
+// public void configureMessageBroker(MessageBrokerRegistry registry){
 
-    registry.setApplicationDestinationPrefixes("/app");
-    registry.enableSimpleBroker("/topic");
-}
-}
+//     registry.setApplicationDestinationPrefixes("/app");
+//     registry.enableSimpleBroker("/topic");
+// }
+// }
