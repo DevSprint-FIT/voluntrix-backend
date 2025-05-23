b172f438324c0cadac586abc27667733532ebe82
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-05-23 19:54:11 +0530

    feat: add new JPQL query to find event apllication by giving event and volunteer entities

--- src/main/java/com/DevSprint/voluntrix_backend/repositories/EventApplicationRepository.java
+++ src/main/java/com/DevSprint/voluntrix_backend/repositories/EventApplicationRepository.java
@@ -1,6 +1,10 @@
 package com.DevSprint.voluntrix_backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.DevSprint.voluntrix_backend.entities.EventApplicationEntity;
 import com.DevSprint.voluntrix_backend.entities.EventEntity;
@@ -9,5 +13,9 @@ import com.DevSprint.voluntrix_backend.entities.VolunteerEntity;
 public interface EventApplicationRepository extends JpaRepository<EventApplicationEntity, Long> {
 
     boolean existsByEventAndVolunteer(EventEntity event, VolunteerEntity volunteer);
+
+    @Query("SELECT ea FROM EventApplicationEntity ea WHERE ea.event = :event AND ea.volunteer = :volunteer")
+    Optional<EventApplicationEntity> findByEventAndVolunteer(@Param("event") EventEntity event,
+            @Param("volunteer") VolunteerEntity volunteer);
 
 }
