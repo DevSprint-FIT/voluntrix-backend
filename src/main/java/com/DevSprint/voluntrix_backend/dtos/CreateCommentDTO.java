7a02240c3ceb435543047a4dd686a5ff5a62540b
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-07-10 11:36:50 +0530

    feat: add comment DTOs and entity for comment functionality

---
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/CreateCommentDTO.java
@@ -0,0 +1,14 @@
+package com.DevSprint.voluntrix_backend.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCommentDTO {
+    private Long socialFeedId;
+    private String userUsername;
+    private String userType;
+    private String content;
+}
