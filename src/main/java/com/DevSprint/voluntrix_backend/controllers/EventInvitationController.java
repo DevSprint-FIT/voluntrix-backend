4cb8899316bbf6847299c5d1db28b7cd061dc7a9
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-15 21:58:22 +0530

    feat: create event invitation table and its CRUD operations

---
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/EventInvitationController.java
@@ -0,0 +1,79 @@
+package com.DevSprint.voluntrix_backend.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.DevSprint.voluntrix_backend.dtos.EventInvitationCreateDTO;
+import com.DevSprint.voluntrix_backend.dtos.EventInvitationDTO;
+import com.DevSprint.voluntrix_backend.services.EventInvitationService;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/public/event-invitations")
+@SecurityRequirement(name = "bearerAuth")
+@RequiredArgsConstructor
+public class EventInvitationController {
+
+    private final EventInvitationService eventInvitationService;
+
+    @GetMapping
+    public ResponseEntity<List<EventInvitationDTO>> getAllEventInvitations() {
+        return new ResponseEntity<List<EventInvitationDTO>>(eventInvitationService.getAllEventInvitations(),
+                HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> addEventInvitation(@RequestBody EventInvitationCreateDTO eventInvitationCreateDTO) {
+        if (eventInvitationCreateDTO == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        eventInvitationService.addEventInvitation(eventInvitationCreateDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{eventInvitationId}")
+    public ResponseEntity<EventInvitationDTO> getEventInvitationById(@PathVariable Long eventInvitationId) {
+        if (eventInvitationId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        var selectedInvitation = eventInvitationService.getEventInvitationById(eventInvitationId);
+        return new ResponseEntity<>(selectedInvitation, HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/{eventInvitationId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateEventInvitation(@PathVariable Long eventInvitationId,
+            @Valid @RequestBody EventInvitationCreateDTO eventInvitationCreateDTO) {
+        if (eventInvitationId == null || eventInvitationCreateDTO == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        eventInvitationService.updateEventInvitation(eventInvitationCreateDTO, eventInvitationId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{eventInvitationId}")
+    public ResponseEntity<Void> deleteEventInvitation(@PathVariable Long eventInvitationId) {
+        if (eventInvitationId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        eventInvitationService.deleteEventInvitation(eventInvitationId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
