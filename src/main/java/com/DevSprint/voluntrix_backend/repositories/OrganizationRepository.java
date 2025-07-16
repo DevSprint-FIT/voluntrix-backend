ed7733b56866a29c027efa68f78bdd229a4e1424
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-16 16:12:10 +0530

    feat: create new endpoint to get only organization id, name, imageUrl

--- src/main/java/com/DevSprint/voluntrix_backend/repositories/OrganizationRepository.java
+++ src/main/java/com/DevSprint/voluntrix_backend/repositories/OrganizationRepository.java
@@ -1,14 +1,19 @@
 package com.DevSprint.voluntrix_backend.repositories;
 
+import com.DevSprint.voluntrix_backend.dtos.OrganizationNameDTO;
 import com.DevSprint.voluntrix_backend.entities.OrganizationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrganizationRepository extends JpaRepository<OrganizationEntity, Long> {
     Optional<OrganizationEntity> findByUsername(String username);
 
+    // JPQL Query
+    @Query("SELECT new com.DevSprint.voluntrix_backend.dtos.OrganizationNameDTO(e.id, e.name, e.imageUrl) FROM OrganizationEntity e")
+    List<OrganizationNameDTO> findAllOrganizationIdNameAndUrl();
 }
-
