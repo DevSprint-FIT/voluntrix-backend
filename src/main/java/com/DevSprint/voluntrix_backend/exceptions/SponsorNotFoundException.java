4043451eb97eaff284f08c422c824387aac090fa
hadithya <hhadithya34@gmail.com>
2025-07-26 23:39:57 +0530

    feat(exception): adde exceptions for sponsorship

---
+++ src/main/java/com/DevSprint/voluntrix_backend/exceptions/SponsorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.DevSprint.voluntrix_backend.exceptions;
+
+public class SponsorNotFoundException extends RuntimeException {
+    public SponsorNotFoundException(String message) {
+        super(message);
+    }
+}
