d8141f0efb5806618f9b47b6652684ae7e6192fe
Nuraj Perera <nurajsp@gmail.com>
2025-07-27 22:33:00 +0530

    update : fixed the timestamp problem

--- src/main/java/com/DevSprint/voluntrix_backend/dtos/ChatMessageDTO.java
+++ src/main/java/com/DevSprint/voluntrix_backend/dtos/ChatMessageDTO.java
@@ -3,6 +3,7 @@ package com.DevSprint.voluntrix_backend.dtos;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +17,7 @@ public class ChatMessageDTO {
     private String receiverId;
     private String content;
     private MessageType type;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime timestamp;
     private MessageStatus status;
 
