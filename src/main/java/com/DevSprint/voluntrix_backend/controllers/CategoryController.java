a12b5eb4d6c9bf984624beb48f63221e442ff014
hadithya <hhadithya34@gmail.com>
2025-07-29 00:49:57 +0530

    feat: added category POST controller to ADMIN API

--- src/main/java/com/DevSprint/voluntrix_backend/controllers/CategoryController.java
+++ src/main/java/com/DevSprint/voluntrix_backend/controllers/CategoryController.java
@@ -1,7 +1,10 @@
 package com.DevSprint.voluntrix_backend.controllers;
 
 import com.DevSprint.voluntrix_backend.dtos.*;
+import com.DevSprint.voluntrix_backend.enums.UserType;
 import com.DevSprint.voluntrix_backend.services.*;
+import com.DevSprint.voluntrix_backend.validation.RequiresRole;
+
 import lombok.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +18,17 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @RequiresRole({UserType.VOLUNTEER, UserType.ORGANIZATION, UserType.ADMIN})
     @GetMapping("/{categoryId}/followers")
     public ResponseEntity<Set<VolunteerCategoryDTO>> getCategoryFollowers(@PathVariable Long categoryId) {
         Set<VolunteerCategoryDTO> followers = categoryService.getCategoryFollowers(categoryId);
         return ResponseEntity.ok(followers);
+    }
+
+    @RequiresRole(UserType.ADMIN)
+    @PostMapping("/")
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryCreateDTO categoryCreateDTO) {
+        CategoryDTO createdCategory = categoryService.createCategory(categoryCreateDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
     }
 }
--- src/main/java/com/DevSprint/voluntrix_backend/services/CategoryService.java
+++ src/main/java/com/DevSprint/voluntrix_backend/services/CategoryService.java
@@ -1,5 +1,7 @@
 package com.DevSprint.voluntrix_backend.services;
 
+import com.DevSprint.voluntrix_backend.dtos.CategoryCreateDTO;
+import com.DevSprint.voluntrix_backend.dtos.CategoryDTO;
 import com.DevSprint.voluntrix_backend.dtos.VolunteerCategoryDTO;
 import com.DevSprint.voluntrix_backend.entities.CategoryEntity;
 import com.DevSprint.voluntrix_backend.repositories.CategoryRepository;
@@ -20,5 +22,16 @@ public class CategoryService {
         CategoryEntity category = categoryRepository.findByIdWithFollowers(categoryId)
                 .orElseThrow(() -> new RuntimeException("Category not found with id: " + categoryId));
         return volunteerCategoryConverter.convertCategoryFollowersToDTOs(category);
+    }
+
+    public CategoryDTO createCategory(CategoryCreateDTO categoryCreateDTO) {
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setCategoryName(categoryCreateDTO.getCategoryName());
+        
+        // Save the new category to the repository
+        CategoryEntity savedCategory = categoryRepository.save(categoryEntity);
+        
+        // Convert the saved entity to DTO
+        return new CategoryDTO(savedCategory.getCategoryId(), savedCategory.getCategoryName());
     }
 }
